58b3424f1aa632800e3539b08f51984c8ce6170c
Emanoel Jesus <157412500+m-emanoel@users.noreply.github.com>
2026-07-31 16:23:12 -0300

    Update roles.js

--- roles.js
+++ roles.js
@@ -39,8 +39,10 @@ const roles = {
 		
 		loading: FLUIGC.loading(window),
 
-		alertTI: FLUIGC.toast({title: 'Erro: ', message: 'Ocorreu um problema. Entre em contato com o TI.', type: 'danger'}),
-		
+		alertTI: (classe) => {
+			FLUIGC.toast({title: 'Erro: ', message: 'Ocorreu um problema. Entre em contato com o TI.', type: 'danger'})
+		},
+	
 		UPPERCASE: (campo) => {
 			$(campo).val(campo.val().toUpperCase());
 		},
